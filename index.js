7a7e680b2f767a0abef9011db1dd44cb97862348
Shadman <shadmanabid87@gmail.com>
2024-01-11 19:01:05 +0600

    update user function has been created to update names , variable length query pari nai tai korte pari ni sob , delete user has been created

--- index.js
+++ index.js
@@ -54,8 +54,36 @@ try {
 });
 
 //update a user
+//updates can be his name , picture or password-ekhon kar jonye dhoree ney name update korbe
+app.put('/users',async(req,res)=>{
+  try {
+    const {id}=req.body;
+    const {first_name,last_name}=req.body;
+    const Query="UPDATE users SET first_name=$1 , last_name=$2 where user_id=$3 ";
+    const updatedUser= await pool.query(Query,[first_name,last_name,id]);
+    res.json("user names have been updated");
+    
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Something went wrong');
+  }
+});
 
-//delete a user
+//delete a user--account deletion of some product deletion
+app.delete('/users',async(req,res)=>{
+  try {
+    const {id}=req.body;
+  const Query="DELETE FROM users WHERE user_id=$1";
+  const deletedUser=await pool.query(Query,[id]);
+  res.json("user has been deleted");
+    
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("something went wrong");
+    
+  }
+
+})
 
 
 
