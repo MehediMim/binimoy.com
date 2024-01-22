9876fbfa3851895db6dbaefd54c664c9c742a70a
Shadman <shadmanabid87@gmail.com>
2024-01-22 10:39:02 +0600

    database code

--- Database.sql
+++ Database.sql
@@ -7,12 +7,14 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE SEQUENCE users_user_id_seq START 1;
 -- Create the users table with hashed passwords
 CREATE TABLE users (
-    User_id INT DEFAULT nextval('users_user_id_seq') PRIMARY KEY,
-    First_Name VARCHAR(255),
-    Middle_Name VARCHAR(255),
-    Last_Name VARCHAR(255),
-    Phone_Number VARCHAR(14),
-    Password VARCHAR(255)
+    user_id INT DEFAULT nextval('users_user_id_seq') PRIMARY KEY,
+    first_Name VARCHAR(100) NOT NULL,
+    middle_Name VARCHAR(100),
+    last_Name VARCHAR(100) NOT NULL,
+    phone_Number VARCHAR(14),   
+    email VARCHAR(255),
+    password_hash VARCHAR(255),
+     CHECK ((phone_number IS NOT NULL AND email IS NULL) OR (phone_number IS NULL AND email IS NOT NULL))
 );
 
 -- Set the sequence to the next available value
