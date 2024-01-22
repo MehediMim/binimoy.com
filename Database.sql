079bb562f1b3951dbaa44fc99b0893a26bb33db7
Shadman <shadmanabid87@gmail.com>
2024-01-22 18:48:49 +0600

    all the tables created, didn't check too much

--- Database.sql
+++ Database.sql
@@ -8,10 +8,10 @@ CREATE SEQUENCE users_user_id_seq START 1;
 -- Create the users table with hashed passwords
 CREATE TABLE users (
     user_id INT DEFAULT nextval('users_user_id_seq') UNIQUE PRIMARY KEY,
-    first_Name VARCHAR(100) NOT NULL,
-    middle_Name VARCHAR(100),
-    last_Name VARCHAR(100) NOT NULL,
-    phone_Number VARCHAR(30) UNIQUE,   
+    first_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100),
+    last_name VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(30) UNIQUE,   
     email VARCHAR(255) UNIQUE,
     password_hash VARCHAR(255),
      CHECK ((phone_number IS NOT NULL AND email IS NULL) OR (phone_number IS NULL AND email IS NOT NULL))
@@ -24,10 +24,12 @@ SELECT setval('users_user_id_seq', COALESCE((SELECT MAX(user_id) FROM users), 1)
 --phone number is a multivalued attribute so another table was needed to maintain the first normal form
 -- Table for User Phone Numbers
 CREATE TABLE user_phone_number (
-    user_id INT,
-    phone_number VARCHAR(30),
+    user_id INT UNIQUE NOT NULL,
+    phone_number VARCHAR(30) UNIQUE,
     PRIMARY KEY (user_id, phone_number),
-    FOREIGN KEY (user_id , phone_Number) REFERENCES users(user_id,phone_number)--not sure though
+    FOREIGN KEY (user_id ) REFERENCES users(user_id),
+    FOREIGN KEY (phone_number) REFERENCES users(phone_number) 
+    --not sure though
 );
 
 --items table , user_id is used as foreign key
@@ -40,12 +42,12 @@ CREATE TABLE items (
     subcategory_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    image BLOB,  --it has to be made correct
+  --  image BLOB,  --it has to be made correct
     price DECIMAL(10, 0) NOT NULL CHECK (price >= 0),
     status VARCHAR(50),
-    date_Posted TIMESTAMP WITH TIMEZONE DEFAULT CURRENT_TIMESTAMP,
+    date_Posted TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (category_id) REFERENCES categories(category_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
 SELECT setval('items_item_id_seq', COALESCE((SELECT MAX(item_id) FROM items), 1), false);
 
@@ -54,7 +56,7 @@ SELECT setval('items_item_id_seq', COALESCE((SELECT MAX(item_id) FROM items), 1)
 CREATE TABLE categories (
     category_id INT  PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
+    description TEXT NOT NULL
 );
 
 
@@ -86,23 +88,24 @@ CREATE TABLE item_discounts (
 );
 --for message 
 -- Table for Messages
-CREATE TABLE messages (
-    message_id INT SERIAL PRIMARY KEY,
+
+CREATE TABLE messages(
     user_id_sender INT NOT NULL,
     user_id_receiver INT NOT NULL,
     message_history TEXT ,
-    message_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    message_date TIMESTAMP WITH TIME ZONE  DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id_sender, user_id_receiver),
     FOREIGN KEY (user_id_receiver) REFERENCES users(user_id),
      FOREIGN KEY (user_id_sender) REFERENCES users(user_id)
 );
 
 --for reporting 
 CREATE TABLE report(
-    report_id INT SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     item_id INT NOT NULL,
     description TEXT,
-    date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_posted TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, item_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (item_id) REFERENCES items(item_id)
 );
@@ -110,14 +113,54 @@ CREATE TABLE report(
 
 --FOR REVIEWS
 CREATE TABLE reviews(
-    review_id INT SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     item_id INT NOT NULL,
     content TEXT,
     upvotes INT DEFAULT 0,
     downvotes INT DEFAULT 0,
-    date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_posted TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, item_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (item_id) REFERENCES items(item_id)
 );
 
+-- Orders Table
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    total_price DECIMAL(10) NOT NULL CHECK (total_price > 0),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE order_details (
+    order_id INT NOT NULL REFERENCES orders(order_id),
+    item_id INT NOT NULL REFERENCES items(item_id),
+    quantity INT NOT NULL CHECK (quantity > 0),
+    price DECIMAL(10,0) NOT NULL CHECK (price > 0),
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+
+CREATE TABLE shipping_info (
+    order_id INT NOT NULL REFERENCES orders(order_id),
+    address_line1 VARCHAR(255),
+    address_line2 VARCHAR(255),
+    city VARCHAR(100),
+    division VARCHAR(100),
+    zip_code VARCHAR(20),
+    shipping_date TIMESTAMP,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+
+CREATE TABLE payments (
+    order_id INT UNIQUE REFERENCES orders(order_id),
+    user_id INT REFERENCES users(user_id),
+    amount DECIMAL(10, 0) NOT NULL CHECK (amount > 0),
+    payment_method VARCHAR(255),
+    PRIMARY KEY (order_id,user_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
